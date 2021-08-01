49e4780494facb6c82ed1cf89593b874417328ef
Susmit Basu <bosesusmit2006@gmail.com>
2021-08-01 23:38:45 +0800

    Bcrypt, Environment config parameter, Config setup

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,28 @@
 from flask import Flask, render_template
+from flask_bcrypt import Bcrypt
+
+from config import app_config
+
+bcrypt = Bcrypt()
+
 
 def create_app(config_name):
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_object(app_config[config_name])
+    app.config.from_pyfile('config.py')
+    bcrypt.init_app(app)
 
     @app.route('/', methods=['GET'])
     def index():
         return render_template('index.html')
+
+    @app.route('/<path:path>', methods=['GET'])
+    def any_root_path(path):
+        return render_template('index.html')
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        pass
+        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
